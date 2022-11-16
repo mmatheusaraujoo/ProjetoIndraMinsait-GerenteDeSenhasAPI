97752882e6422ad15be1e0c1b61fd281efb26307
Matheus Araújo <matheus10_araujo@hotmail.com>
2022-11-16 18:38:38 -0300

    Montando Esqueleto
    Montando esqueleto do projeto com conexão ao banco de dados MySql local

---
+++ GerenteDeSenhas/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using GerenteDeSenhas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GerenteDeSenhas.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> otps) : base (otps)
+        {
+           
+        }
+
+        private DbSet<Senha> Senhas { get; set; }
+        
+    }
+
+}
---
+++ GerenteDeSenhas/Models/Senha.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GerenteDeSenhas.Models
+{
+    public class Senha
+    {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        public string Descricao { get; set; }
+        [Required]
+        public string Valor { get; set; }
+        [Required]
+        public string TipoDeCriptografia { get; set; }
+    }
+}
